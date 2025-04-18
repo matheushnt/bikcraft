51b38db3d42f16003bc2c97f76e24fceef926d0f
Matheus Henrique <matheushnt06@gmail.com>
2025-04-18 11:26:30 -0300

    Parâmetros URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll('.header-menu a');
 console.log(links);
 
@@ -11,3 +12,15 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
